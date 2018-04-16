d0bd4ba7e29db26269f904e1f231baa210be97ed
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-17 07:52:27 +0800

    2018 Updates

---
+++ Conversions/problem13.php
@@ -0,0 +1,8 @@
+&gt;?php
+ //Problem 13: Write a program that converts celsius to farenheit.
+ $celsius = 10.0;
+ $farenheit = 0.0;
+ $farenheit = (9.0 / 5.0) * $celsius + 32;
+ echo $celsius."C equals to ".
+  $farenheit."F";
+?>
