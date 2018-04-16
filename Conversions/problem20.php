0f02740914e8837f11d434c6860a91afae216576
Arjun Araneta <arjunaraneta@gmail.com>
2018-04-17 07:54:25 +0800

    2018 Updates

---
+++ Conversions/problem20.php
@@ -0,0 +1,10 @@
+&gt;?php
+ //Problem 20: Write a program that converts a number to its 
+ //corresponding month (e.g. 1 = January).
+ $index = 1;
+ $month = array("January", "February", "March", "April",
+  "May", "June", "July", "August",
+  "September", "October", "November", "December");
+
+ echo $month[$index - 1];
+?>
