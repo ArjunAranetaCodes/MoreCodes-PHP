7fbbca2d898701e54cc41059750b354bc99f00d8
Arjun Araneta <arjunaraneta@gmail.com>
2018-11-20 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/problem1.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Problem 1: Write a program to print the number pattern of ones and zeros using loop.
+11111
+11111
+11111
+11111
+11111
+*/
+ for($y = 0; $y < 5; $y++){
+  for($x = 0; $x < 5; $x++){
+   echo "1";
+  }
+  echo "<br/>";
+ }
+?>
